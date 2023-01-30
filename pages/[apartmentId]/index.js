f86fcb50d334dc1260d31d3a61fccd0fd962d83a
Cheong Tze Yuan <tzeyuan220@gmail.com>
2023-01-30 11:13:58 +0800

    update fallback

--- pages/[apartmentId]/index.js
+++ pages/[apartmentId]/index.js
@@ -23,7 +23,7 @@ export async function getStaticPaths(){
 
   client.close();
     return {
-        fallback: blocking,
+        fallback: 'blocking',
         paths: apartments.map(appartment => ({
              params: {apartmentId: appartment._id.toString() },
             })),
